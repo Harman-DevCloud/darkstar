6ccbec7cf802daedd213eed2f45a4378ce2dcbbb
IkennaOkeke <ikennaaokeke@gmail.com>
2025-01-28 19:32:07 -0700

    Star object

---
+++ app/src/main/java/com/example/darkstar/Star.java
@@ -0,0 +1,14 @@
+package com.example.darkstar;
+
+public class Star extends Shape {
+    private String colour;
+    private int x;
+    private int y;
+    public Star() {
+        
+    colour = "Red";
+
+    }
+
+
+}
